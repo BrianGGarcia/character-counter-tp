96e3e4e6e155b5f002e6210f4f9a7c1dfe9336fc
briangarcia <rocknrolla91@gmail.com>
2026-07-30 14:59:53 -0300

    add: letter density

--- src/App.jsx
+++ src/App.jsx
@@ -8,10 +8,12 @@ const App = () => {
   const [excludeSpaces, setExcludeSpaces] = useState(false)
   const [limitCharacter, setLimitCharacter] = useState(false)
   const [limitValue, setLimitValue] = useState(300)
+  const [showAll, setShowAll] = useState(false)
 
   const character = excludeSpaces ? text.replace(/\s/g, "").length : text.length
   const words = text.trim() === "" ? 0 : text.trim().split(/\s+/).length
   const sentences = text.trim() === "" ? 0 : text.split(/[.!?]/).filter(sentence => sentence.trim() !== "").length
+  const readingTime = Math.ceil(words / 200)
 
   const handleChangeTextArea = (e) => {
     const value = e.target.value
@@ -30,6 +32,32 @@ const App = () => {
     const newText = text.slice(0, limitValue)
     setText(newText)
   }
+
+  const cleanText = text.toLowerCase().replace(/[^a-záéíóúñü]/g, "")
+  const total = cleanText.length
+  // diccionario → clave/valor
+  const dictionaryLetters = {}
+
+  cleanText.split("").forEach(letter => {
+    dictionaryLetters[letter] = (dictionaryLetters[letter] || 0) + 1
+  })
+
+  const letters = Object.entries(dictionaryLetters).map(dataLetter => {
+    const letter = dataLetter[0]
+    const amountLetter = dataLetter[1]
+
+    const infoToRenderLetter = {
+      letterName: letter,
+      amount: amountLetter,
+      percentage: (amountLetter / total) * 100
+    }
+
+    return infoToRenderLetter
+  })
+
+  const sortLetters = letters.sort((a, b) => b.amount - a.amount)
+
+  // const visibleLetters = showAll ? sortLetters : sortLetters.slice(0, 5)
 
   return (
     <main>
@@ -71,6 +99,35 @@ const App = () => {
       <p>Cantidad de caracteres: {character}</p>
       <p>Cantidad de palabras: {words}</p>
       <p>Cantidad de oraciones: {sentences}</p>
+      <p>Tiempo de lectura: ~ {readingTime} minute</p>
+      <section>
+        <h2>Cantidad de letras</h2>
+        {/* <button onClick={() => setShowAll(!showAll)}>{showAll ? "Ver menos ▲" : "Ver mas ▼"}</button> */}
+        <article>
+          {
+            sortLetters.slice(0, 5).map(letter => (
+              <div key={letter.letterName}>
+                <span>{letter.letterName.toUpperCase()}</span>
+                <meter min="0" max="100" value={letter.percentage}></meter>
+                <span>{letter.amount} ({letter.percentage.toFixed(1)} % )</span>
+              </div>))
+          }
+        </article>
+
+        <details>
+          <summary>See more</summary>
+          <ul className="meter-list">
+            {
+              sortLetters.slice(5, sortLetters.length).map(letter => (
+                <div key={letter.letterName}>
+                  <span>{letter.letterName.toUpperCase()}</span>
+                  <meter min="0" max="100" value={letter.percentage}></meter>
+                  <span>{letter.amount} ({letter.percentage.toFixed(1)}%)</span>
+                </div>))
+            }
+          </ul>
+        </details>
+      </section>
     </main>
   )
 }
